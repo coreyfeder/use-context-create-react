69685c55ae379062c00ff53e73ba973d680c088b
Corey Feder <corey.feder@gmail.com>
2024-04-02 13:57:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ComponentA from './ComponentA';
+
+function App() {
+  return (
+    <div className="App">
+      <ComponentA />
+    </div>
+  );
+}
+
+export default App;
